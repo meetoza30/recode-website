d7cd948eb466e0a7918f329f473ebaa724eb91ab
meetoza30 <meetoz305@gmail.com>
2025-10-06 23:21:34 +0530

    design updated

--- src/components/InterviewPrep/.history/Backend/src/config/gemini_20251004160121.js
+++
@@ -1,7 +0,0 @@
-import { GoogleGenerativeAI } from "@google/generative-ai";
-import "dotenv/config";
-
-console.log(process.env.GEMINI_API_KEY);
-const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
-
-export const embeddingModel = genAI.getGenerativeModel({ model: "embedding-001" });
--- src/components/InterviewPrep/.history/Backend/src/config/pinecone_20251004160110.js
+++
@@ -1,9 +0,0 @@
-import { Pinecone } from '@pinecone-database/pinecone';
-import "dotenv/config";
-
-const pc = new Pinecone({
-  apiKey: process.env.PINECONE_API_KEY
-});
-const index = pc.index('ty-project');
-
-export {index, pc}
